b59000e3e3d92acd83ded8fe58737b4f02eb3851
Julianna Messineo <160360849+mathematiCode@users.noreply.github.com>
2025-03-20 10:32:39 -0500

    Set up env variable for port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get('/api', (req, res) => {
